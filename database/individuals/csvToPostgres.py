4e412db0a8ff89ce295b874c774e65e950be361a
John Verhoff <jverhoff@purdue.edu>
2018-12-03 16:16:12 -0500

    Individual contributions csv to Postgres

--- database/individuals/csvToPostgres.py
+++ database/individuals/csvToPostgres.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://jverhoff:john@localhost:5432/fecInfo')
+#engine = create_engine('postgresql://jverhoff:john@localhost:5432/fecInfo')
+engine = create_engine('postgresql://databasemaster:Purdue2020!@mydbinstance.cfaohzw41elj.us-east-2.rds.amazonaws.com:5432/databasemaster')
 df = pd.read_csv('indiv_header_file.csv', low_memory=False)
 df.columns = [c.lower() for c in df.columns]
 df.to_sql("contribution_table", engine)
-for df1 in pd.read_csv('indivCont.csv', chunksize=500):
+for df1 in pd.read_csv('indivCont.csv', chunksize=50000, error_bad_lines=False, warn_bad_lines=False):
     df1.columns = [c.lower() for c in df.columns]
     df1.to_sql("contribution_table", engine, if_exists='append')
