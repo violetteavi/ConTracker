459dcd5aaa92162041c4c877fc9e0f5d7dcdd1d3
John Verhoff <jverhoff@purdue.edu>
2018-12-03 01:10:17 -0500

    database loading works maybe

--- database/individuals/csvToPostgres.py
+++ database/individuals/csvToPostgres.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://jverhoff:john@localhost:5432/fecInfo')
-#df = pd.read_csv('indiv_header_file.csv', low_memory=False)
-#df.columns = [c.lower() for c in df.columns]
-#df.to_sql("contribution_table", engine)
-for df in pd.read_csv('indivCont.csv', chunksize=500):
-    df.columns = [c.lower() for c in df.columns]
-    df.to_sql("contribution_table", engine, if_exists='append')
+df = pd.read_csv('indiv_header_file.csv', low_memory=False)
+df.columns = [c.lower() for c in df.columns]
+df.to_sql("contribution_table", engine)
+for df1 in pd.read_csv('indivCont.csv', chunksize=500):
+    df1.columns = [c.lower() for c in df.columns]
+    df1.to_sql("contribution_table", engine, if_exists='append')
