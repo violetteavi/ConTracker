7b12f466dea1c49639d56a6df18ad97314e15cb2
JoshuaSchmitt <schmit28@purdue.edu>
2018-12-03 21:18:59 -0500

    Revert "Current working instance of server"
    This reverts commit 9b4249dd4e6dba0411765a62a1a9e77fbea0b515.

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from database.queryDatabase import getStateData 
+from ConTrackerFetcher import getStateData 
 from ConTrackerHtmlGen import generatePieHtmlBegin
 from ConTrackerHtmlGen import generatePieHtmlDataStr
 from ConTrackerHtmlGen import generatePieHtmlEnd
@@ -14,7 +14,7 @@ def writeHTMLforStates():
 
 	htmlContent = generatePieHtmlBegin();
 	for state in states:	
-		(totalContState,DemPercent,RepPercent,OtherPercent) = getStateData(state)
+		(statePercentage,totalContState,DemPercent,RepPercent,OtherPercent) = getStateData(state)
 		
 		
 		htmlContent = htmlContent + generatePieHtmlDataStr(state,totalContState,DemPercent,OtherPercent,RepPercent)  
